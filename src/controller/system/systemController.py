d9a1fd402717cc27f3033d41b044cf49eb10f9b2
Felipe Toffoli <felipe_toffoli1@hotmail.com>
2020-10-05 15:45:33 -0400

    add controller  system

--- src/controller/system/systemController.py
+++ src/controller/system/systemController.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, current_app
 from flask_jwt_extended import  jwt_required
 from src.infra.decorator.auth.authDecorator import AuthDecorator
 from src.infra.decorator.request.errorsRequestDecorator import ErrorsRequestDecorator
+from src.handler.system.systemHandler import SystemHandler as System
 
 
 controller_system = Blueprint('controller_system', __name__)
@@ -13,6 +14,29 @@ validity_req = ErrorsRequestDecorator()
 @controller_system.route('/system', methods=['GET'])
 @jwt_required
 def get_all():
-    param1 = request.args.get('param1')
-    param2 = request.args.get('param2')
-    return jsonify([param1, param2])
+    return  System().get_all() 
+
+@controller_system.route('/system/params', methods=['GET'])
+@jwt_required
+def get_by_params():
+    return  System().get_by_params() 
+
+@controller_system.route('/system', methods=['PUT'])
+@validity_req.body_is_json
+@jwt_required
+def update():
+    return  System().update()
+
+@controller_system.route('/system', methods=['POST'])
+@jwt_required
+@validity_req.body_is_json
+def create():
+    return  System().create()
+
+@controller_system.route('/system', methods=['DELETE'])
+@jwt_required
+@validity_req.body_is_json
+def delete_by_id():
+    return  System().delete() 
+
+
